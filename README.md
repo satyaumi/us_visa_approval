33490a63196418dfb572098355733c97a999f33d
satyaumi <satyamlkinf@gmail.com>
2024-10-19 12:08:26 +0530

    requirements changes

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 # create the project folder structure
 
 - flowchart:
+
+# create envirnment 
+conda create -n visa python=3.8-y
+
+conda activate visa
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup,find_packages
+
+setup(
+     name ="us_visa",
+     version="0.0.0",
+     author="satya",
+     author_email="satyamlkinf@gmail.com",
+     packages=find_packages()
+)
