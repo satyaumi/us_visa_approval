30757ed7307bcea2958ad69fc0cad0133d3ff8f2
satyaumi <satyamlkinf@gmail.com>
2024-10-23 18:22:43 +0530

    data ingestion added

--- README.md
+++ README.md
@@ -8,3 +8,12 @@
 conda create -n visa python=3.8-y
 
 conda activate visa
+
+
+# workflow
+1. constants
+2. entity
+3. components
+4. pipeline
+
+
--- demo.py
+++ demo.py
@@ -1,9 +1,3 @@
-from us_visa.logger import logging
-import sys
-from us_visa.exception import USvisaException
-# logging.info("welcome to our custom log")
-
-try:
-    a =2/0
-except Exception as e:
-     raise USvisaException(e,sys)
+from us_visa.pipline.training_pipeline import TrainPipeline
+obj =TrainPipeline()
+obj.run_pipeline()
